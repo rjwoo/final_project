e796facda500cfa0495f1b2c97982e49464ab5fb
Ryan Woo <rjwoo@sfu.ca>
2016-08-04 10:54:38 -0700

    log form now in exercise index

--- app/controllers/exercise_logs_controller.rb
+++ app/controllers/exercise_logs_controller.rb
@@ -1,14 +1,12 @@
 class ExerciseLogsController < ApplicationController
 
   def new
-    @program = Program.find params[:program_id]
     @exercises = @program.exercises
     @exercise_log = ExerciseLog.new
   end
 
   def create
-    @program = Program.find params[:program_id]
-    @exercises = @program.exercises
+    @exercise = Exercise.find params[:exercise_id]
     @exercise_log = ExerciseLog.new exercise_logs_params
     if @exercise_log.save
       redirect_to programs_path, notice: "Exercise Completed!"
