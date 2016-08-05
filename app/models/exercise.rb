373bfdd0799e4b8f3b2c6d70ec5df198e7b23cab
Ryan Woo <rjwoo@sfu.ca>
2016-08-05 11:13:55 -0700

    able to display one graph

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -4,4 +4,20 @@ class Exercise < ActiveRecord::Base
   belongs_to :exercise_option
   belongs_to :program
 
+  def exercise_date
+    date = []
+    self.exercise_logs.each do |f|
+      date << f.created_at.strftime("%F")
+    end
+    date
+  end
+
+  def weight_lifted
+    weight = []
+    self.exercise_logs.each do |l|
+      weight << l.weight
+    end
+    weight
+  end
+
 end
--- app/models/program.rb
+++ app/models/program.rb
@@ -2,4 +2,6 @@ class Program < ActiveRecord::Base
   has_many :exercises, dependent: :destroy
 
   validates :title, presence: true, uniqueness: true
+
+  
 end
